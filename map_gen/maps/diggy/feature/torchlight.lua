980695855fbea5af8a8b98d73f469180ad4246f3
Sascha Kuehl <253145410+Sascha-Kuehl@users.noreply.github.com>
2026-01-10 09:49:23 +0100

    diggy: feature torchlight - introduced torchlight inventory

--- map_gen/maps/diggy/feature/torchlight.lua
+++ map_gen/maps/diggy/feature/torchlight.lua
@@ -1,14 +1,19 @@
 local Event = require 'utils.event'
 local Global = require 'utils.global'
 local Gui = require 'utils.gui'
-local RS = require 'map_gen.shared.redmew_surface'
+local Table = require 'utils.table'
 
 local TICK_INTERVAL = 60
 local TICKS_PER_WOOD = 60 * 60 * 1
 local AFTERBURNER_TICKS = 60 * 17
 local LIGHT_SCALE = 2.0
+local INITIAL_WOOD_COUNT = 10
 
-local torchlight_button_name = Gui.uid_name()
+local torchlight_frame_name = Gui.uid_name()
+local torchlight_enabled_button_name = Gui.uid_name()
+local torchlight_flow_name = Gui.uid_name()
+local torchlight_slot_button_name = Gui.uid_name()
+local torchlight_progressbar_name = Gui.uid_name()
 
 -- "map" from player index to {enabled, remaining_ticks, light_ids}
 local player_light_data = {}
@@ -16,62 +21,121 @@ local player_light_data = {}
 -- "map" from position to {remaining_ticks, light_ids}
 local corpse_light_data = {}
 
+-- "map" from player index to torchlight inventory
+local torchlight_inventory = {}
+
 Global.register(
-    {
-        player_light_data = player_light_data,
-        corpse_light_data = corpse_light_data,
-    },
-    function(tbl)
-        player_light_data = tbl.player_light_data
-        corpse_light_data = tbl.corpse_light_data
-    end
+        {
+            player_light_data = player_light_data,
+            corpse_light_data = corpse_light_data,
+            torchlight_inventory = torchlight_inventory
+        },
+        function(tbl)
+            player_light_data = tbl.player_light_data
+            corpse_light_data = tbl.corpse_light_data
+            torchlight_inventory = tbl.torchlight_inventory
+        end
 )
 
 local Torchlight = {}
 
+function realign_torchlight_frame(player)
+    local frame = player.gui.screen[torchlight_frame_name]
+
+    local resolution = player.display_resolution
+    local scale = player.display_scale
+
+    frame.location = { 190 * scale, resolution.height - (96 * scale) }
+end
+
+function update_torchlight_progressbar(player)
+    local progressbar = player.gui.screen[torchlight_frame_name][torchlight_flow_name][torchlight_progressbar_name]
+    local remaining_ticks = player_light_data[player.index].remaining_ticks
+
+    local remaining_percent = (remaining_ticks - AFTERBURNER_TICKS) / TICKS_PER_WOOD
+    if (remaining_percent < 0) then
+        remaining_percent = 0
+    end
+
+    progressbar.value = remaining_percent
+    progressbar.tooltip = tostring(remaining_percent * TICKS_PER_WOOD / 60) .. ' sec'
+end
+
 function create_gui_button(player)
-    Gui.add_top_element(player, {
+    local frame = player.gui.screen.add {
+        type = 'frame',
+        name = torchlight_frame_name,
+        direction = 'vertical'
+    }
+    frame.style.padding = 0
+
+    local enabled_button = frame.add {
         type = 'sprite-button',
-        name = torchlight_button_name,
-        sprite = 'achievement/pyromaniac',
-        tooltip = 'Turn on/off torch light',
-        auto_toggle = true,
-        toggled = player_light_data[player.index].enabled
-      })
+        name = torchlight_enabled_button_name,
+        tooltip = 'Switch light on/off',
+        sprite = 'virtual-signal/signal-sun',
+        auto_toggle = true, toggled = player_light_data[player.index].enabled,
+        style = 'quick_bar_page_button'
+    }
+    enabled_button.style.width = 38
+    enabled_button.style.height = 38
+
+    local flow = frame.add {
+        type = 'flow',
+        name = torchlight_flow_name,
+        direction = 'vertical'
+    }
+    flow.style.vertical_spacing = 0
+
+    local slot_button = flow.add {
+        type = 'sprite-button',
+        name = torchlight_slot_button_name,
+        sprite = 'virtual-signal/signal-fire',
+        style = 'tool_equip_ammo_slot'
+    }
+    slot_button.style.width = 38
+    slot_button.style.height = 38
+
+    local progressbar = flow.add {
+        type = 'progressbar',
+        name = torchlight_progressbar_name,
+        value = 0.0
+    }
+    progressbar.style.width = 38
+
+    realign_torchlight_frame(player)
+    update_torchlight_progressbar(player)
 end
 
 function create_main_light(target, surface)
-    return rendering.draw_light
-        {
-          sprite = 'utility/light_medium',
-          color = {250,200,120},
-          surface = surface,
-          target = target,
-        }
+    return rendering.draw_light {
+        sprite = 'utility/light_medium',
+        color = { 250, 200, 120 },
+        surface = surface,
+        target = target,
+    }
 end
 
 function create_effect_light_1(target, surface)
-    return rendering.draw_light
-        {
-            sprite = 'utility/light_medium',
-            color = {170,40,0},
-            surface = surface,
-            target = target,
-            intensity = 0.25,
-            blink_interval = 11
-        }
+    return rendering.draw_light {
+        sprite = 'utility/light_medium',
+        color = { 170, 40, 0 },
+        surface = surface,
+        target = target,
+        intensity = 0.25,
+        blink_interval = 11
+    }
 end
 
 function create_effect_light_2(target, surface)
-    return rendering.draw_light
-       {
-           sprite = 'utility/light_medium',
-           color = {200,100,0},
-           surface = surface,
-           target = target,
-           intensity = 0.25,
-           blink_interval = 13
-       }
+    return rendering.draw_light {
+        sprite = 'utility/light_medium',
+        color = { 200, 100, 0 },
+        surface = surface,
+        target = target,
+        intensity = 0.25,
+        blink_interval = 13
+    }
 end
 
 function create_or_restore_player_light(player)
@@ -84,7 +148,7 @@ function create_or_restore_player_light(player)
         light_data = {
             enabled = true,
             remaining_ticks = 0,
-            light_ids = {main_light.id, effect_light_1.id, effect_light_2.id}
+            light_ids = { main_light.id, effect_light_1.id, effect_light_2.id }
         }
         player_light_data[player.index] = light_data
         return
@@ -109,6 +173,12 @@ function create_or_restore_player_light(player)
     end
 
     return
+end
+
+function create_player_torchlight_inventory(player)
+    local inventory = game.create_inventory(1)
+    inventory.insert({ name = 'wood', count = INITIAL_WOOD_COUNT })
+    torchlight_inventory[player.index] = inventory
 end
 
 function get_intensity(remaining_ticks)
@@ -144,16 +214,6 @@ function update_light(light_data)
     effect_light_2.scale = LIGHT_SCALE * 1.15 * intensity
 end
 
-function remove_one_wood(inventory, player)
-    inventory.remove({name = 'wood', count = 1})
-    player.create_local_flying_text
-    {
-        text = '-1 wood',
-        surface = player.surface,
-        position = player.position,
-    }
-end
-
 function update_player_light(player)
     if (player.ticks_to_respawn ~= nil) then
         return
@@ -168,16 +228,17 @@ function update_player_light(player)
         return
     end
 
-    local inventory = player.character.get_inventory(defines.inventory.character_main)
+    local inventory = torchlight_inventory[player.index]
     local woodCount = inventory.get_item_count('wood')
 
     -- player has wood, so we can burn it
     if (woodCount ~= 0) then
-        remove_one_wood(inventory, player)
+        inventory.remove({ name = 'wood', count = 1 })
         light_data.remaining_ticks = TICKS_PER_WOOD + AFTERBURNER_TICKS
     end
 
     update_light(light_data)
+    update_inventory_button(player)
 end
 
 function update_player_lights_on_tick()
@@ -188,18 +249,18 @@ function update_player_lights_on_tick()
             light_data.remaining_ticks = 0
         end
         update_player_light(player)
+        update_torchlight_progressbar(player)
 
         -- player runs out of wood so we show a message
         if (light_data.enabled
                 and light_data.remaining_ticks > 0
                 and light_data.remaining_ticks < AFTERBURNER_TICKS
                 and light_data.remaining_ticks % 180 == 0) then
-            player.create_local_flying_text
-            {
+            player.create_local_flying_text {
                 text = 'no more wood',
                 surface = player.surface,
                 position = player.position,
-                color = {250, 0, 0}
+                color = { 250, 0, 0 }
             }
         end
     end
@@ -233,8 +294,9 @@ function on_player_created(event)
     player.disable_flashlight()
 
     create_or_restore_player_light(player)
-    update_player_light(player)
+    create_player_torchlight_inventory(player)
     create_gui_button(player)
+    update_player_light(player)
 end
 
 function on_player_respawned(event)
@@ -285,7 +347,7 @@ end
 
 function on_torchlight_button_pressed(event)
     local player = event.player
-    local button = Gui.get_top_element(player, torchlight_button_name)
+    local button = player.gui.screen[torchlight_frame_name][torchlight_enabled_button_name]
     player_light_data[player.index].enabled = button.toggled
     update_player_light(player)
 end
@@ -306,6 +368,96 @@ function configure_wood_in_market()
     })
 end
 
+function on_player_display_resolution_changed(event)
+    local player = game.get_player(event.player_index)
+    realign_torchlight_frame(player)
+end
+
+function on_player_display_scale_changed(event)
+    local player = game.get_player(event.player_index)
+    realign_torchlight_frame(player)
+end
+
+function update_inventory_button(player)
+    local inventory = torchlight_inventory[player.index]
+    local slot_button = player.gui.screen[torchlight_frame_name][torchlight_flow_name][torchlight_slot_button_name]
+
+    local stack = inventory[1];
+
+    if (stack.count == 0) then
+        slot_button.sprite = 'virtual-signal/signal-fire'
+        slot_button.number = nil
+    else
+        slot_button.sprite = 'item/' .. stack.name
+        slot_button.number = stack.count
+    end
+end
+
+function handle_inventory_slot_click(item_stack, click_event, allowed_items)
+    local player = game.get_player(click_event.player_index)
+    local main_inventory = player.character.get_main_inventory()
+    local cursor_stack = player.cursor_stack
+    if (cursor_stack == nil) then
+        return
+    end
+
+    if (player.is_cursor_empty()) then
+        if (not item_stack.valid_for_read) then
+            return
+        end
+
+        local pick_count
+        if (click_event.button == defines.mouse_button_type.left) then
+            pick_count = item_stack.count
+        elseif (click_event.button == defines.mouse_button_type.right) then
+            pick_count = item_stack.count / 2
+        else
+            return
+        end
+
+        -- when holding Ctrl or Shift, then the chosen amount will be directly transferred to the main inventory
+        if (click_event.control or click_event.shift) then
+            local insert_count = main_inventory.get_insertable_count(item_stack)
+            if (insert_count < pick_count) then
+                pick_count = insert_count
+            end
+            -- don't know how to transfer a specific amount to main inventory, so the cursor_stack is used as "buffer"
+            cursor_stack.transfer_stack(item_stack, pick_count)
+            main_inventory.insert(cursor_stack)
+            cursor_stack.clear()
+        else
+            cursor_stack.transfer_stack(item_stack, pick_count)
+        end
+    else
+        if (not Table.contains(allowed_items, cursor_stack.name)) then
+            return
+        end
+
+        if (click_event.button == defines.mouse_button_type.left and (not item_stack.valid_for_read or cursor_stack.name ~= item_stack.name or cursor_stack.quality ~= item_stack.quality)) then
+            item_stack.swap_stack(cursor_stack)
+            return
+        end
+
+        local push_count
+        if (click_event.button == defines.mouse_button_type.left) then
+            push_count = cursor_stack.count
+        elseif (click_event.button == defines.mouse_button_type.right) then
+            push_count = 1
+        else
+            return
+        end
+
+        item_stack.transfer_stack(cursor_stack, push_count)
+    end
+end
+
+function on_torchlight_fuel_pressed(event)
+    local player = game.get_player(event.player_index)
+    local inventory = torchlight_inventory[player.index]
+    handle_inventory_slot_click(inventory[1], event, { 'wood' })
+    update_inventory_button(player)
+end
+
 function Torchlight.register()
     Event.add(defines.events.on_player_created, on_player_created)
     Event.add(defines.events.on_player_respawned, on_player_respawned)
@@ -313,8 +465,11 @@ function Torchlight.register()
     Event.add(defines.events.on_player_died, on_player_died)
     Event.add(defines.events.on_player_main_inventory_changed, on_player_main_inventory_changed)
     Event.add(defines.events.on_tick, on_tick)
-    
-    Gui.on_click(torchlight_button_name, on_torchlight_button_pressed)
+    Event.add(defines.events.on_player_display_resolution_changed, on_player_display_resolution_changed)
+    Event.add(defines.events.on_player_display_scale_changed, on_player_display_scale_changed)
+
+    Gui.on_click(torchlight_enabled_button_name, on_torchlight_button_pressed)
+    Gui.on_click(torchlight_slot_button_name, on_torchlight_fuel_pressed)
 end
 
 function Torchlight.on_init()
